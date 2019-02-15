90ca94b307fb6c168a9287dbda1e7d6be8342c3b
龙猫 <foxmaillucien@126.com>
2019-02-15 16:29:16 +0800

    增加了appID和appsecret

--- src/Config/MessageConfig.java
+++ src/Config/MessageConfig.java
@@ -9,6 +9,8 @@
  *       Description:
  */
 public interface MessageConfig {
-	public static final String token = "wechatReceiveTonken";//与配置界面的ToKen一致
-
+	String token = "wechatReceiveTonken";//与配置界面的ToKen一致
+//	appID和appsecret每个开发账号均不同，请修改为自己账号所对应的变量
+	String appID = "wx849d9ec6361711f8";
+	String appsecret = "43c48b279635a856ac869e6cf758ee6f";
 }
