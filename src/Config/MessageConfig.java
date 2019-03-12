70baaeaa294ef67f7058a85166107f7228b40e31
龙猫 <foxmaillucien@126.com>
2019-03-13 01:25:51 +0800

    增加了PROJECT_NAME常量，方便日后部署服务器更改项目名称，并将文件中其他静态变量关联已定义常量

--- src/Config/MessageConfig.java
+++ src/Config/MessageConfig.java
@@ -13,11 +13,14 @@ public interface MessageConfig {
 //	appID和appsecret每个开发账号均不同，请修改为自己账号所对应的变量
 	String appID = "wx849d9ec6361711f8";
 	String appsecret = "43c48b279635a856ac869e6cf758ee6f";
+	//项目名称
+	String PROJECT_NAME = "wechatAutoResponder";
 	//服务器前缀，请改为自己的测试服务器
-	String servletURL = "http://lonmao.iok.la/wechatAutoResponder";
+	String servletURL = "http://lonmao.iok.la/" + PROJECT_NAME;
 	//自定义菜单json文件的文件名，路径为项目根目录下的Json目录，不能修改，否则会抛出（FileNotFoundException）异常
 	String CUSTOMIZE_MENUS = "Json" + System.getProperty ("file.separator") + "Menu.json";
 
+
 	//执行查询成绩和考勤操作时验证未绑定账号返回信息
-	String UNLOGINMESSAGE = "你尚未绑定账号，请先绑定账号！<a href='https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx849d9ec6361711f8&redirect_uri=" + servletURL + "/PIM/Register/login.jsp&response_type=code&scope=snsapi_base&state=123#wechat_redirect'>账号绑定</a>";
+	String UNLOGINMESSAGE = "你尚未绑定账号，请先绑定账号！<a href='https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + appID + "&redirect_uri=" + servletURL + "/PIM/Register/login.jsp&response_type=code&scope=snsapi_base&state=123#wechat_redirect'>账号绑定</a>";
 }
